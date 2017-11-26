ce0ffe1bc2b5ec31c5d083b59afa7a53ee23fe39
Phil Perry <phil4597@catskilltech.com>
2017-11-26 18:42:17 -0500

    3.008 build

--- lib/PDF/Builder.pm
+++ lib/PDF/Builder.pm
@@ -5,7 +5,7 @@ no warnings qw[ deprecated recursion uninitialized ];
 
 # $VERSION defined here so developers can run PDF::Builder from git.
 # it should be automatically updated as part of the CPAN build.
-$our $VERSION = '3.008'; # VERSION
+our $VERSION = '3.008'; # VERSION
 my $LAST_UPDATE = '3.008'; # manually update whenever code is changed
 
 use Carp;
