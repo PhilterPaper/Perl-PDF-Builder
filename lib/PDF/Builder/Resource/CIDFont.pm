453661f07941fa90a41d8e46f13daec0ee3aeae5
Phil Perry <phil4597@catskilltech.com>
2025-04-17 07:10:23 -0900

    part of fix #193

--- lib/PDF/Builder/Resource/CIDFont.pm
+++ lib/PDF/Builder/Resource/CIDFont.pm
@@ -228,9 +228,14 @@ sub text {
     #      1 (default) = x20 and same/longer spaces
     #      2 = all spaces
     #      the problem is, other font types handle only x20 in Reader
-    my $latest_page = $self->{' apipdf'}->{' outlist'}[0]->{'Pages'}->{'Kids'}->{' val'}[-1];
-    my $wordspace = $latest_page->{'Contents'}->{' val'}->[1]->{' wordspace'};
-    my $fontsize = $latest_page->{'Contents'}->{' val'}->[1]->{' fontsize'};
+    my ($latest_page, $wordspace, $fontsize);
+    $latest_page = $self->{' apipdf'}->{' outlist'}[0]->{'Pages'}->{'Kids'}->{' val'}[-1];
+    $wordspace = $latest_page->{'Contents'}->{' val'}->[1]->{' wordspace'};
+    $fontsize = $latest_page->{'Contents'}->{' val'}->[1]->{' fontsize'};
+if (!defined $wordspace || !defined $fontsize || $fontsize <= 0) {
+    $wordspace = $latest_page->{'Contents'}->{' val'}->[0]->{' wordspace'};
+    $fontsize = $latest_page->{'Contents'}->{' val'}->[0]->{' fontsize'};
+}
     my @fragments = ( $text ); # default for wordspace = 0
     # TBD: get list of different lengths of spaces found, split on all of them
     #      could have null fragments where two or more spaces in a row, or
